b1bb671da639e971bc71b5073ec9474aa6df284b
Sajjad5591 <sajjadirfan5591@gmail.com>
2025-07-04 23:07:44 -0700

    Add project files.

---
+++ muti purpos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class StoreNameAndAge
+{
+    static void Main()
+    {
+        string name;
+        int age;
+
+        Console.WriteLine("=== Store and Display Name & Age ===");
+
+        Console.Write("Enter your name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("=== Output ===");
+        Console.WriteLine($"Your Name: {name}");
+        Console.WriteLine($"Your Age: {age}");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
